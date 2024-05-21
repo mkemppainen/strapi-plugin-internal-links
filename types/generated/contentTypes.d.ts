d7a44b0d48b8bb20a893f66da6105098de2648ea
MikeVanEssen <mike@webbio.nl>
2024-05-21 10:02:56 +0200

    chore: removed settings

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -646,11 +646,11 @@ export interface ApiPagePage extends Schema.CollectionType {
 				'plugin::internal-links.internal-link',
 				{
 					noTitle: true;
-					enableExternalApi: true;
-					slug: 'f';
-					externalApi: 'http://localhost:3000';
-					externalApiLabelMapping: 'startPointReference';
-					externalApiValueMapping: 'question';
+					enableExternalApi: false;
+					slug: '';
+					externalApi: '';
+					externalApiLabelMapping: '';
+					externalApiValueMapping: '';
 				}
 			> &
 			Attribute.SetPluginOptions<{
